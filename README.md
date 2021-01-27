99d616c05be5c9e4928c69131500854c7e887946
Joshua Wilder Oakley <43791165+jwoakley@users.noreply.github.com>
2021-01-27 15:40:27 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## Hello World for P5.js
+#### by Joshua Wilder Oakley
+
+
+
+### Overview
+This is A Hello World Sketch
+
+
+### Technical Details
+
---
+++ sketch.js
@@ -0,0 +1,15 @@
+
+// Setup code goes here
+function setup() {
+  createCanvas(1920, 1080);
+  textSize(80);
+  textAlign(Center);
+ }
+
+
+// Draw code goes here
+function draw() {
+  background(0);
+  fill(200, 100, 30);
+  text("Hello World", width/2, height/2);
+}
